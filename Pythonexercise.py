fe3c3054a8921f0dc1b5424c5e16ef916e43b5e6
Knifaharan <55033947+Knifaharan@users.noreply.github.com>
2019-09-24 22:55:13 +0530

    If Else Sum

--- Pythonexercise.py
+++ Pythonexercise.py
@@ -1,6 +1,5 @@
-def monkey_trouble(a_smile, b_smile):
-  if a_smile and b_smile:
-    return True
-  if not a_smile and not b_smile:
-    return True
-  return False
+def sum_double(a, b):
+  if a is b:
+    return 2 * (a + b)
+  else:
+    return a+b
