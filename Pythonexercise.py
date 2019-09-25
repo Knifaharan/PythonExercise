79c00bd83f5d6295c5cafd919cac60bc4860c5ed
Knifaharan <55033947+Knifaharan@users.noreply.github.com>
2019-09-25 20:58:17 +0530

    Strings

--- Pythonexercise.py
+++ Pythonexercise.py
@@ -1,4 +1,12 @@
-def First_two(str):
-    if len(str) <=2:
-        return str
-        return str[:2]
+def First_half(str):
+    return str[:len(str)/2]
+def without_end(str):
+    return str[1:-1]
+def combo_string(a, b):
+    if len(a) > len(b):
+        return b + a + b
+        return a + b + a
+def non_start(a, b):
+    return a[1:] + b[1:]
+def left2(str):
+    return str[2:] + str[:2]
